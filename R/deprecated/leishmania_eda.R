a057c4f049d8f6a393b4a0060148cec75346d0ac
Harrison Wang <harrisonized@gmail.com>
2023-09-14 12:03:16 -0400

    move code comments from schistosoma_eda to leishmania_eda, add some arguments to schistosoma_eda

--- R/deprecated/leishmania_eda.R
+++ R/deprecated/leishmania_eda.R
@@ -7,6 +7,7 @@ wd = dirname(dirname(this.path::here()))  # wd = '~/github/diy-transcriptomics'
 suppressMessages(library('EnsDb.Hsapiens.v86'))
 # suppressMessages(library('BSgenome.Mfuro.UCSC.musFur1'))
 suppressMessages(library('GenomicFeatures'))
+library('tibble')
 library('optparse')
 library('logr')
 source(file.path(wd, 'R', 'utils.R'))  # list_files, filter_list_for_match
@@ -21,9 +22,9 @@ option_list = list(
                 metavar="data/leishmania/mapped_reads", type="character",
                 help="set the input directory"),
 
-    make_option(c("-f", "--file-ext"), default="h5",
-                metavar="h5", type="character",
-                help="Choose: 'h5' or 'tsv'. 'h5' is faster for importing data"),
+    make_option(c("-f", "--filename"), default="abundance.h5",
+                metavar="abundance.h5", type="character",
+                help="Choose: 'abundance.h5' or 'abundance.tsv'. '.h5' is faster for importing data"),
 
     make_option(c("-t", "--troubleshooting"), default=FALSE, action="store_true",
                 metavar="FALSE", type="logical",
@@ -43,13 +44,37 @@ log_print(paste('Script started at:', start_time))
 # ----------------------------------------------------------------------
 # Read Data
 
+
 # define data source
 files = filter_list_for_match(
     list_files(file.path(wd, opt['input-dir'][[1]])),
-    opt['file-ext'][[1]]  # file_ext
+    opt['filename'][[1]]  # file_ext
 )
-tx2gene_obj <- GenomicFeatures::transcripts(EnsDb.Hsapiens.v86, columns=c("tx_id", "gene_name"))
+# Example file: "data/schistosoma/mapped_reads/F12h_LE_1/abundance.tsv"
+#   target_id    length eff_length est_counts   tpm
+#   <chr>         <dbl>      <dbl>      <dbl> <dbl>
+# 1 Smp_186980.1    402      239.       171   58.5 
+# 2 Smp_197050.1    186       50.3       59   95.8 
+# 3 Smp_160500.1   5316     5152.       244    3.87
 
+
+# get human annotations
+tx2gene_obj <- as_tibble(
+    GenomicFeatures::transcripts(EnsDb.Hsapiens.v86, columns=c("tx_id", "gene_name"))
+)  
+tx2gene_obj <- dplyr::rename(tx2gene_obj, target_id = tx_id)  # change first column name to 'target_id'
+tx2gene_obj <- dplyr::select(tx2gene_obj, "target_id", "gene_name")  # transcript ID needs to be the first column in the dataframe
+
+
+# Read abundance files into a txi object
+# see: https://github.com/mikelove/tximport/blob/devel/R/tximport.R
+# names(txi)
+# [1] "abundance"  "counts"  "infReps"  "length"  "countsFromAbundance
+# txi$abundance = tpm
+# txi$counts != counts, they did something
+# txi$infReps: inferential replicates, not sure what this is
+# txi$length = eff_length
+# txi$countsFromAbundance = "lengthScaledTPM"
 sample_txi <- tximport::tximport(
     files,  # load data from mapped_reads
     type = "kallisto", 
@@ -58,6 +83,7 @@ sample_txi <- tximport::tximport(
     countsFromAbundance = "lengthScaledTPM",
     ignoreTxVersion = TRUE
 )
+
 
 # ----------------------------------------------------------------------
 # Peform EDA here
--- R/schistosoma_eda.R
+++ R/schistosoma_eda.R
@@ -1,19 +1,18 @@
-## Source: Step1_TxImport.R, Step2_dataWrangling
-## 1. Concat data
-## 2. Filter and normalization
-## 3. Plot
+## Adapted from: Step1_TxImport.R, Step2_dataWrangling
+## Concatenates the abundance files in the schistosoma dataset and creates some violin plots
 
 wd = dirname(this.path::here())  # wd = '~/github/diy-transcriptomics'
 # library(tidyverse)  # too broad
 library('readr')  # read_tsv
-library('tibble')  # as_tibble
+suppressMessages(library('dplyr'))
 library('tidyr')  # pivot_longer
-library('matrixStats')  # colSums, rowSums
+library('tibble')  # as_tibble
+suppressMessages(library('matrixStats'))  # colSums, rowSums
 library('ggplot2')  # ggplot
 library('cowplot')  # plot_grid
-
 library('tximport')  # tx_import
 suppressMessages(library('EnsDb.Hsapiens.v86'))
+suppressMessages(library('GenomicFeatures'))
 suppressMessages(library('edgeR'))  # DGElist, cpm
 library('optparse')
 library('logr')
@@ -25,14 +24,17 @@ source(file.path(wd, 'R', 'utils.R'))  # list_files, filter_list_for_match
 
 # args
 option_list = list(
-
     make_option(c("-i", "--input-dir"), default="data/schistosoma/mapped_reads",
-                metavar="data/leishmania/mapped_reads", type="character",
+                metavar="data/schistosoma/mapped_reads", type="character",
                 help="set the input directory"),
 
-    make_option(c("-f", "--file-ext"), default="h5",
-                metavar="h5", type="character",
-                help="Choose: 'h5' or 'tsv'. 'h5' is faster for importing data"),
+    make_option(c("-f", "--filename"), default="abundance.h5",
+                metavar="abundance.h5", type="character",
+                help="Choose: 'abundance.h5' or 'abundance.tsv'. '.h5' is faster for importing data"),
+
+    make_option(c("-o", "--output-dir"), default="figures/schistosoma",
+                metavar="figures/schistosoma", type="character",
+                help="set the output directory for the figures"),
 
     make_option(c("-s", "--save"), default=TRUE, action="store_false", metavar="TRUE",
                 type="logical", help="disable if you're troubleshooting and don't want to overwrite your files")
@@ -49,40 +51,27 @@ log_print(paste('Script started at:', start_time))
 
 
 # ----------------------------------------------------------------------
-# Concat data
-# Adapted from Step1_TxImport.R
+# Read Data
 
 log_print(paste(Sys.time(), 'Reading files...'))
 
+# define data source
+# schistosoma dataset has 144 files
+files = filter_list_for_match(
+    list_files(file.path(wd, opt['input-dir'][[1]])),
+    opt['filename'][[1]]  # file_ext
+)
+
 # get human annotations
 tx2gene_obj <- as_tibble(
-    GenomicFeatures::transcripts('EnsDb.Hsapiens.v86', columns=c("tx_id", "gene_name"))
-)
-tx2gene_obj <- dplyr::rename(tx2gene_obj, target_id = tx_id)  #need to change first column name to 'target_id'
-tx2gene_obj <- dplyr::select(tx2gene_obj, "target_id", "gene_name")  #transcript ID needs to be the first column in the dataframe
-
-# Example file: "data/schistosoma/mapped_reads/F12h_LE_1/abundance.tsv"
-#   target_id    length eff_length est_counts   tpm
-#   <chr>         <dbl>      <dbl>      <dbl> <dbl>
-# 1 Smp_186980.1    402      239.       171   58.5 
-# 2 Smp_197050.1    186       50.3       59   95.8 
-# 3 Smp_160500.1   5316     5152.       244    3.87
-abundance_filepaths = filter_list_for_match(
-    list_files(file.path(wd, "data", "schistosoma", "mapped_reads")),
-    'abundance.h5'
-)
+    GenomicFeatures::transcripts(EnsDb.Hsapiens.v86, columns=c("tx_id", "gene_name"))
+)  
+tx2gene_obj <- dplyr::rename(tx2gene_obj, target_id = tx_id)  # change first column name to 'target_id'
+tx2gene_obj <- dplyr::select(tx2gene_obj, "target_id", "gene_name")  # transcript ID needs to be the first column in the dataframe
 
 # Read abundance files into a txi object
-# see: https://github.com/mikelove/tximport/blob/devel/R/tximport.R
-# names(txi)
-# [1] "abundance"  "counts"  "infReps"  "length"  "countsFromAbundance
-# txi$abundance = tpm
-# txi$counts != counts, they did something
-# txi$infReps: inferential replicates, not sure what this is
-# txi$length = eff_length
-# txi$countsFromAbundance = "lengthScaledTPM"
 txi <- tximport(
-    abundance_filepaths,
+    files,
     type = "kallisto", 
     tx2gene = tx2gene_obj, 
     txOut = TRUE,  # doesn't work when this is FALSE
@@ -90,8 +79,7 @@ txi <- tximport(
     ignoreTxVersion = TRUE
 )
 
-
-# update column names, they're not included
+# update column names
 sample_ids = unlist(lapply(abundance_filepaths, function(x) basename(dirname(x))))
 colnames(txi$counts) <- unlist(lapply(sample_ids, function(x) paste(x, '_count', sep='')))
 colnames(txi$abundance) <- unlist(lapply(sample_ids, function(x) paste(x, '_abundance', sep='')))
@@ -99,19 +87,24 @@ colnames(txi$length) <- unlist(lapply(sample_ids, function(x) paste(x, '_length'
 
 if (save==TRUE) {
     log_print(paste(Sys.time(), 'Saving txi files...'))
-    if (!dir.exists(file.path(wd, 'data', 'schistosoma', 'txi'))) {
-        dir.create(file.path(wd, 'data', 'schistosoma', 'txi'), recursive=TRUE)
+    if (!dir.exists(file.path(wd, dirname(opt['input-dir'][[1]]), 'txi'))) {
+        dir.create(file.path(wd, dirname(opt['input-dir'][[1]]), 'txi'), recursive=TRUE)
     }
     write.table(txi$counts,
-                file.path(wd, 'data', 'schistosoma', 'txi', 'txi_counts.csv'),
+                file.path(wd, dirname(opt['input-dir'][[1]]), 'txi', 'txi_counts.csv'),
                 quote=FALSE, col.names=TRUE, row.names=FALSE, sep=',')
     write.table(txi$abundance,
-                file.path(wd, 'data', 'schistosoma', 'txi', 'txi_abundances.csv'),
+                file.path(wd, dirname(opt['input-dir'][[1]]), 'txi', 'txi_abundances.csv'),
                 quote=FALSE, col.names=TRUE, row.names=FALSE, sep=',')
     # write.table(txi$length,
-    #             file.path(wd, 'data', 'schistosoma', 'txi', 'txi_lengths.csv'),
+    #             file.path(wd, dirname(opt['input-dir'][[1]]), 'txi', 'txi_lengths.csv'),
     #             quote=FALSE, col.names=TRUE, row.names=FALSE, sep=',')
 }
+
+
+# ----------------------------------------------------------------------
+# Transform data
+# Adapted from Step2_dataWrangling
 
 # Add SD, AVG, and MED as columns to the txi$abundance tibble for plotting later
 txi$abundance <- transform(
@@ -121,20 +114,14 @@ txi$abundance <- transform(
     MED=rowMedians(txi$abundance)
 )
 
-
-# ----------------------------------------------------------------------
-# Transform data
-# Adapted from Step2_dataWrangling
-
-
 # Create DGElist
 # See: https://www.rdocumentation.org/packages/edgeR/versions/3.14.0/topics/DGEList-class
 dge_list <- edgeR::DGEList(txi$counts)
 
 # note: this doesn't work for some reason
 if (FALSE) {
-    save(dge_list, file=file.path(wd, 'data', 'schistosoma', "dge_list.txt"))
-    load(file=file.path(wd, 'data', 'schistosoma', "dge_list.txt"))  # example load
+    save(dge_list, file=file.path(wd, dirname(opt['input-dir'][[1]]), "dge_list.txt"))
+    load(file=file.path(wd, dirname(opt['input-dir'][[1]]), "dge_list.txt"))  # example load
 }
 
 # unfiltered, non-normalized cpm
@@ -149,7 +136,6 @@ log2.cpm.df.pivot <- pivot_longer(
     names_to = "samples", # name of that new variable (column)
     values_to = "expression"
 )
-
 
 # filtered, non-normalized cpm
 # table(rowSums(dge_list$counts==0)==10)
@@ -178,7 +164,7 @@ if (save==TRUE) {
     log_print(paste(Sys.time(), 'Saving filtered, normalized cpm...'))
     write.table(
         log2.cpm.filtered.norm.df,
-        file.path(wd, 'data', 'schistosoma', 'filtered_normalized_cpm.csv'),
+        file.path(wd, dirname(opt['input-dir'][[1]]), 'filtered_normalized_cpm.csv'),
         quote=FALSE, col.names=TRUE, row.names=FALSE, sep=','
     )
 }
@@ -193,7 +179,7 @@ log2.cpm.filtered.norm.df.pivot <- pivot_longer(
 
 
 # ----------------------------------------------------------------------
-# Plot
+# Plot figures
 # Adapted from Step2_dataWrangling
 
 
@@ -213,7 +199,7 @@ fig <- ggplot(txi$abundance) +
     theme_bw()
 
 if (save==TRUE) {
-    ggsave(file.path(wd, 'figures', 'schistosoma', 'eda', 'tpm_unfiltered_nonnormalized.png'),
+    ggsave(file.path(wd, opt['input-dir'][[1]], 'eda', 'tpm_unfiltered_nonnormalized.png'),
            height=750, width=1200, dpi=300, units="px", scaling=0.5)
 }
 
@@ -236,7 +222,7 @@ p1 <- ggplot(log2.cpm.df.pivot) +
        ) +
   theme_bw()
 if (save==TRUE) {
-    ggsave(file.path(wd, 'figures', 'schistosoma', 'eda', 'cpm_unfiltered_nonnormalized.png'),
+    ggsave(file.path(wd, opt['input-dir'][[1]], 'eda', 'cpm_unfiltered_nonnormalized.png'),
            height=750, width=1200, dpi=300, units="px", scaling=0.5)
 }
 
@@ -260,7 +246,7 @@ p2 <- ggplot(log2.cpm.filtered.df.pivot) +
 # Also: try using coord_flip() at the end of the ggplot code
 
 if (save==TRUE) {
-    ggsave(file.path(wd, 'figures', 'schistosoma', 'eda', 'tpm_filtered_nonnormalized.png'),
+    ggsave(file.path(wd, opt['input-dir'][[1]], 'eda', 'tpm_filtered_nonnormalized.png'),
            height=750, width=1200, dpi=300, units="px", scaling=0.5)
 }
 
@@ -282,7 +268,7 @@ p3 <- ggplot(log2.cpm.filtered.norm.df.pivot) +
        caption=paste0("produced on ", Sys.time())) +
   theme_bw()
 if (save==TRUE) {
-    ggsave(file.path(wd, 'figures', 'schistosoma', 'eda', 'tpm_filtered_normalized.png'),
+    ggsave(file.path(wd, opt['input-dir'][[1]], 'eda', 'tpm_filtered_normalized.png'),
            height=750, width=1200, dpi=300, units="px", scaling=0.5)
 }
 
@@ -290,7 +276,7 @@ if (save==TRUE) {
 log_print(paste(Sys.time(), 'Plotting cowplot...'))
 cowplot::plot_grid(p1, p2, p3, labels = c('A', 'B', 'C'), label_size = 12)
 if (save==TRUE) {
-    ggsave(file.path(wd, 'figures', 'schistosoma', 'eda', 'cowplot.png'),
+    ggsave(file.path(wd, opt['input-dir'][[1]], 'eda', 'cowplot.png'),
            height=750, width=1200, dpi=300, units="px", scaling=0.5)
 }
 
